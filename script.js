a746004f6dd7ac089c054d00814ac8e6d4a9d7a2
arifbsl <ahak.bsl@gmail.com>
2021-01-31 00:42:33 +0600

    get input values from form input and store in global variables

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
 
+
+let countdownTitle = '';
+let countdownDate = '';
 // Set Date Input MIn with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+
+// Take Values from Form Input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+
+}
+
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
